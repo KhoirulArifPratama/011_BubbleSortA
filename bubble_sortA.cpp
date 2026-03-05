e2cbbe7f0eafa62ad32934ba505e3a7d26bc16a9
KhoirulArifPratama <khoirularif932@gmail.com>
2026-03-05 15:27:36 +0700

    Menambahkan Prosedur Input

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -4,3 +4,18 @@ using namespace std; // Library
 
 int qarr[20]; // Deklarasi Variabel global
 int n;
+
+void input()
+{
+    int d; // Procedure input
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        {
+            cout << "Error! number should be in range of 1-20." << endl;
+        }
+    }
